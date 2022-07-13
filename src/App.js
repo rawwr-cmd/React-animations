b810680de03fc293c23a1fed8b02625e4c5e6c16
rawwr <akshayace0302@gmail.com>
2022-07-13 21:50:22 +0530

    transition events

--- src/App.js
+++ src/App.js
@@ -37,9 +37,15 @@ class App extends Component {
         <br />
         <Transition
           in={this.state.showBlock}
-          timeout={300}
+          timeout={1000}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
         >
           {(state) => (
             <div
